93a463c29ac300bce2ed67de15f51a56faf32593
Jitesh Manglani <jjmanglani01@gmail.com>
2024-04-21 20:43:51 +0530

    Complete functionality phase 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Papa from 'papaparse';
+
+  let bOldData = false;
+  let bNewData = false;
+  let holdingsData: any = {};
+
+  function onChange(data: string, event: Event) {
+    const element = event.target as HTMLInputElement;  
+    const files = element.files;
+    if (files && files[0]) {
+      parseFile(files[0], data);
+    }
+  }
+
+  function parseFile(file: File, data: string) {
+    Papa.parse(file, {
+	    header: true,
+	    skipEmptyLines: true, //other option is 'greedy', meaning skip delimiters, quotes, and whitespace.
+	    columns: null, //or array of strings,
+      complete: function (results: any) {
+        const holdings = results.data;
+        if (data === "old") {
+          bOldData = true;
+          for (let holding of holdings) {
+            holdingsData[holding["ISIN"]] = holding;
+          }
+        } else if (data === "new") {
+          bNewData = true;
+          for (let holding of holdings) {
+            if (holdingsData[holding["ISIN"]]) {
+              const oldHolding = holdingsData[holding["ISIN"]];
+              const change = parseFloat(holding["Previous Closing Price"]) - parseFloat(oldHolding["Previous Closing Price"]);
+              const changePerct = change * 100 / parseFloat(oldHolding["Previous Closing Price"]);
+              holdingsData[holding["ISIN"]] = {...oldHolding, "Change": change, "Change Perct": changePerct };
+            }
+          }
+        }
+      },
+	    error: function (error: any) {
+        console.log("Error:", error);
+      },
+    });   
+  }
+</script>
+<div>
+    <div>
+        <span>Input old holding</span>
+        <input type="file" placeholder="Enter old holding" on:change={onChange.bind(null, "old")} />
+    </div>
+    <div>
+        <span>Input new holding</span>
+        <input type="file" placeholder="Enter new holding" on:change={onChange.bind(null, "new")} />
+    </div>
+    {#if bOldData && bNewData}
+      <table>
+        {#each Object.values(holdingsData) as holding}
+          <tr>
+            <td>{holding["Symbol"]}</td>
+            <td>{holding["Change"]}</td>
+            <td>{holding["Change Perct"]}</td>
+          </tr>
+        {/each}
+      </table>
+    {/if}
+</div>
